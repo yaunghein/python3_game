781fb9f175372175c28954b7c161b84adddd1587
NannapusMinnie <nannapus.tiya@gmail.com>
2026-01-14 22:42:30 +0700

    graphic engine

--- engines/graphics.py
+++ engines/graphics.py
@@ -1,2 +1,22 @@
 class GraphicsEngine:
-    pass
+    def render_state(self, objects):
+
+        for obj in objects:
+            if obj.shape == "circle":
+                symbol = "●"
+            elif obj.shape == "square":
+                symbol = "■"
+            else:
+                symbol = "?"
+
+            print(f"{symbol} {obj.name} at ({obj.x}, {obj.y})")
+
+class test_obj:
+    def __init__(self):
+        self.x = 10
+        self.y = 20
+        self.shape = "triangle"
+        self.name = "TestObject"
+
+g = GraphicsEngine()
+g.render_state([test_obj()])
