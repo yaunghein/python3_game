77a5122ae47f3f0bf7930935052db613fc2f04d0
Boat <bkotchasarn@gmail.com>
2026-01-16 07:00:30 +0700

    Complete collision handling

--- characters.py
+++ characters.py
@@ -12,27 +12,27 @@ class Character:
 class Player(Character):
     def __init__(self, x, y, speed_x=1, speed_y=1, radius=8):
         super().__init__(x, y, speed_x, speed_y, radius)
+        self.mass = 0  # the player is immovable except via keyboard input
 
-    def move(self, left, right, up, down, game_field):
+    def move(self, left, right, up, down):
         self.pos.x += self.vel.x * right - self.vel.x * left
         self.pos.y += self.vel.y * down - self.vel.y * up
 
-        # TODO: Change GameField
-        self.pos, _, _ = game_field.clamp(self.pos)
+        # self.pos, _, _ = game_field.clamp(self.pos)
 
 
 class NPC(Character):
     def __init__(self, x, y, speed_x=1, speed_y=1, radius=8):
         super().__init__(x, y, speed_x, speed_y, radius)
 
-    def move(self, game_field):
+    def move(self):
         self.pos += self.vel
 
         # Move this to game engine
-        self.pos, x_edge, y_edge = game_field.clamp(self.pos)
+        # self.pos, x_edge, y_edge = game_field.clamp(self.pos)
 
-        if x_edge:
-            self.vel.x = -self.vel.x
+        # if x_edge:
+        #     self.vel.x = -self.vel.x
 
-        if y_edge:
-            self.vel.y = -self.vel.y
+        # if y_edge:
+        #     self.vel.y = -self.vel.y
--- game_engine.py
+++ game_engine.py
@@ -4,6 +4,7 @@ import pygame
 from pygame import Vector2
 
 from characters import Character
+from utils import GameField
 
 class GameEngine:
     def __init__(self, graph_engine, game_field, player, npcs, *, fps=60):
@@ -16,22 +17,36 @@ class GameEngine:
         self.fps = fps
 
     def update_state(self, keys):
-        for i in len(self.npcs):
-            npc = self.npcs[i]
-            self.npc.move(self.game_field)
+        for i in range(len(self.npcs)):
+            self.npcs[i].move()
 
         self.player.move(keys[pygame.K_a], keys[pygame.K_d],
-                         keys[pygame.K_w], keys[pygame.K_s], self.game_field)
+                         keys[pygame.K_w], keys[pygame.K_s])
         
-        for i in range(len(self.npcs)):
-            self.handle_collision(self.player, self.npcs[i])
-            for j in range(i, len(self.npcs)):
-                self.handle_collision(self.npcs[i], self.npcs[j])
+        bottom_left = Vector2(self.game_field.x_min, self.game_field.y_min)
+        bottom_right = Vector2(self.game_field.x_max, self.game_field.y_min)
+        top_right = Vector2(self.game_field.x_max, self.game_field.y_max)
+        top_left = Vector2(self.game_field.x_min, self.game_field.y_max)
+
+        walls = [(bottom_left, bottom_right), (bottom_right, top_right),
+                 (top_right, top_left), (top_left, bottom_left)]
+
+        ITER = 10
+        for _ in range(ITER):
+            for i in range(len(self.npcs)):
+                self.handle_collision(self.player, self.npcs[i])
+                for j in range(i, len(self.npcs)):
+                    self.handle_collision(self.npcs[i], self.npcs[j])
+            
+            for wall_a, wall_b in walls:
+                self.handle_wall_collision(self.player, wall_a, wall_b)
+                for i in range(len(self.npcs)):
+                    self.handle_wall_collision(self.npcs[i], wall_a, wall_b)
 
         if keys[pygame.K_q]:
             self.running = False
     
-    def handle_collision(char_1: Character, char_2: Character, slop=0.01, percent=0.8):
+    def handle_collision(self, char_1: Character, char_2: Character, slop=0.01, percent=0.8):
         delta = char_1.pos - char_2.pos
         dist = delta.length()
         sum_of_radii = char_1.radius + char_2.radius
@@ -54,6 +69,7 @@ class GameEngine:
         if inv_mass_sum == 0.0:
             return True  # both immovable but overlapping; nothing to do
         
+        # prevent jittering (somehow)
         correction_mag = max(penetration - slop, 0.0) * percent / inv_mass_sum
         correction = correction_mag * collision_normal
 
@@ -76,15 +92,55 @@ class GameEngine:
         char_2.vel -= impulse * inv_mass_2
 
         return True
+    
+
+    def handle_wall_collision(self, char: Character, wall_a: Vector2, wall_b: Vector2, 
+                              slop=0.01, percent=0.8):
+        wall_segment = wall_b - wall_a
+
+        # Closest point on wall segment to charecter
+        t = (char.pos - wall_a).dot(wall_segment) / (wall_segment.length() ** 2)
+        t = max(0.0, min(1.0, t))
+        C = wall_a + t * wall_segment
+
+        # Vector from closest point to center
+        delta = char.pos - C
+        if delta.length() >= char.radius:
+            return False  # no collision
+        
+        if delta.length() == 0:
+            collision_normal = Vector2(-wall_segment.y, wall_segment.x)
+            if collision_normal.length() == 0:
+                collision_normal = Vector2(1, 0)
+            else:
+                collision_normal = collision_normal.normalize()
+        else:
+            collision_normal = delta / delta.length()
+        
+        # --- 1) Positional correction
+        penetration = char.radius - delta.length()
+        correction_mag = max(penetration - slop, 0.0) * percent
+        char.pos += collision_normal * correction_mag
+
+        # --- 2) Velocity update
+        normal_vel = char.vel.dot(collision_normal)
+
+        if normal_vel < 0:
+            # bouncing into a wall
+            e = 0.5  # restitution
+            char.vel += - (1.0 + e) * normal_vel * collision_normal
+
+        return True
+
 
     def render_state(self):
         self.graph_engine.start_frame()
 
         self.graph_engine.render_circle(
-            self.player.x, self.player.y, self.player.radius, "red")
+            self.player.pos, self.player.radius, "red")
         
         for npc in self.npcs:
-            self.graph_engine.render_circle(self.npc.x, self.npc.y, self.npc.radius, "blue")
+            self.graph_engine.render_circle(npc.pos, npc.radius, "blue")
 
         self.graph_engine.show_frame()
 
