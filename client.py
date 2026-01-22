01258f3c9e2e39cd44e6d002f60ff7b175e2b3b4
Boat <bkotchasarn@gmail.com>
2026-01-22 09:07:07 +0700

    Create a new client.

--- client.py
+++ client.py
@@ -4,18 +4,23 @@ from action import PlayerActions
 
 import socket
 import pygame
+from pygame import Vector2
+import pickle
 
 HOST = '127.0.0.1'
 PORT = 21001
 
 running = True
+fps = 30
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    this_player_id = pickle.loads(s.recv(2048))
     print("Connected to server")
 
     engine = GraphicsEngine()  # pygame.init() is inside
+    clock = pygame.time.Clock()
 
     while running:
         actions = PlayerActions()
@@ -44,16 +49,49 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         print("Waiting for state...")
         state_data = s.recv(1024)
-        state = eval(state_data.decode())
+        world_state = eval(state_data.decode())
 
         # {1: (97, 3), 'self': 1}
-        print("State received:", state)
+        print("State received:", world_state)
 
+        if world_state:
+            engine.start_frame()
+
+            # draw all players
+            for p_id, p_data in world_state["players"].items():
+                color = "red" if p_id == this_player_id else "white"
+                engine.render_circle(
+                    Vector2(p_data["pos"][0], p_data["pos"][1]), p_data["radius"], color)
+                
+                # --- draw player orientation ---
+                start = Vector2(p_data["pos"][0], p_data["pos"][1])
+                direction = p_data["vel"]  ## TODO: add this elsewhere
+
+                if direction.length() > 0:
+                    end = start + direction.normalize() * 30
+                    pygame.draw.line(
+                        engine.screen,
+                        "yellow",
+                        start,
+                        end,
+                        3
+                    )
+
+            # draw all NPCs
+            for npc in world_state["npcs"]:
+                engine.render_circle(
+                    npc["pos"], npc["radius"], "blue")
+
+            # --- Draw bullets ---
+            for bullet in world_state["bullets"]:
+                engine.render_circle(
+                    bullet["pos"],
+                    bullet["radius"],
+                    "yellow"
+                )
+
+            engine.show_frame()
+
+        clock.tick(fps)
         engine.start_frame()
-
-        for player_id, positions in state.items():
-            if player_id == "self":
-                continue
-            engine.render_circle(positions[0], positions[1], 20, "green" if player_id == state["self"] else "blue")
-
         engine.show_frame()
